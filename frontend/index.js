c48fc0332d666d2f4b5f4037a45fa9a49a1794d5
naveenkumarnumber <naveenkumarnumber@gmail.com>
2026-08-13 16:34:22 +0530

    toogle

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,28 @@
 //alert ("welcome to my website");
+var users = [
+    {
+        name:"John Doe", 
+        gender:"male",
+        image:"image.png",
+    },
+    {
+        name:"Jane Doe", 
+        gender:"female",
+        image:"jane.png",
+    }
+]
+var curid = 0;
+function toggleUser(){
+
+        
+    curid = (curid + 1) % 2;
+    var username = document.getElementById("user-name");
+    var usergender = document.getElementById("user-gender");        
+    var userimage = document.getElementById("user-image");
+
+    username.innerHTML = users[curid].name;
+    usergender.innerHTML = users[curid].gender;
+    userimage.src = users[curid].image;
+}
+    
+    
